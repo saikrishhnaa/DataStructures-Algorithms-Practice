68b07633a63acf1061c67025b1f2d09d2bcc8188
saikrishhnaa <saikrishnaamakam@gmail.com>
2023-05-06 00:22:35 +0530

    Array and String - Day 12, Day 13 and Day 16 programs submission

---
+++ LeetCode Programs/1. Array And String/Day16/Maximum Value of a String in an Array.java
@@ -0,0 +1,67 @@
+/*
+Description:
+
+The value of an alphanumeric string can be defined as:
+
+The numeric representation of the string in base 10, if it comprises of digits only.
+The length of the string, otherwise.
+Given an array strs of alphanumeric strings, return the maximum value of any string in strs.
+
+Example 1:
+
+Input: strs = ["alic3","bob","3","4","00000"]
+Output: 5
+Explanation: 
+- "alic3" consists of both letters and digits, so its value is its length, i.e. 5.
+- "bob" consists only of letters, so its value is also its length, i.e. 3.
+- "3" consists only of digits, so its value is its numeric equivalent, i.e. 3.
+- "4" also consists only of digits, so its value is 4.
+- "00000" consists only of digits, so its value is 0.
+Hence, the maximum value is 5, of "alic3".
+Example 2:
+
+Input: strs = ["1","01","001","0001"]
+Output: 1
+Explanation: 
+Each string in the array has value 1. Hence, we return 1.
+ 
+Constraints:
+
+1 <= strs.length <= 100
+1 <= strs[i].length <= 9
+strs[i] consists of only lowercase English letters and digits.
+*/
+
+class Solution {
+    public int maximumValue(String[] strs) {
+        int maxValue = 0;
+        for(String str : strs) {
+            boolean containsAlpha = false;
+            for(char ch : str.toCharArray()) {
+                if(ch >= 97 && ch <= 122) {
+                    if(maxValue < str.length()) {
+                        maxValue = str.length();
+                    }
+                    containsAlpha = true;
+                    break;
+                }
+            }
+
+            if(!containsAlpha && maxValue < Integer.parseInt(str)) {
+                maxValue = Integer.parseInt(str);
+            }
+        }
+        return maxValue;
+    }
+}
+
+/*
+Performance:
+
+Runtime
+1 ms (92.83% Beats)
+
+Memory
+40.4 MB (53.37% Beats)
+*/
+
